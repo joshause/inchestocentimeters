099b3d6e1dd1075179cdf445ec621174fac55ae9
Josh Ause <joshua.ause@gmail.com>
2016-08-06 14:51:45 -0700

    Strings using doublequotes.

--- inchestocentimeters.js
+++ inchestocentimeters.js
@@ -1,19 +1,19 @@
 (function() {
 
-  'use strict';
+  "use strict";
 
   var InchesToCentimeters = function() {};
 
   InchesToCentimeters.prototype.get = function(x) {
     if(typeof x !== "number") {
-      return 'invalid input';
+      return "invalid input";
     } else {
       return x / 0.393700787402;
     }
   };
 
-  if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
+  if (typeof exports !== "undefined") {
+    if (typeof module !== "undefined" && module.exports) {
       exports = module.exports = InchesToCentimeters;
     } else {
       exports.InchesToCentimeters = InchesToCentimeters;
